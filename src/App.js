b1e0513927ac05d9e776bcc2f6cb297ae2fa04e2
mu41555 <usama41555@gmail.com>
2021-10-18 12:46:59 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './myComponents/myTodo';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+    <Todo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/myComponents/myTodo.js
@@ -0,0 +1,54 @@
+import MyPlan from './myPlan';
+import { useState } from 'react';
+
+function Todo() {
+    const [list, setlist] = useState("");
+    const [item, setitem] = useState([]);
+
+
+    const addlist = () => {
+        setitem((myItem) => {
+            return [...myItem, list];
+        })
+        setlist("");
+    }
+
+    const deleteItem = (id) => {
+        setitem((myItem) => {
+            return myItem.filter((arr, i) => {
+                return i !== id;
+            })
+        })
+    }
+    return (
+        <>
+            <div className="container-fluid my-5">
+                <div className="row">
+                    <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+                        <h2 className="text-center">To do App</h2>
+                        <div className="row">
+                            <div className="col-10">
+                                <input type="text" className="form-control" placeholder="Write Plan Here" value={list} onChange={(e) => { setlist(e.target.value) }} />
+                            </div>
+                            <div className="col-1">
+                                <button className="btn btn-warning px-" onClick={addlist}><i className="fas fa-plus bg-warning fs-5"></i></button>
+                            </div>
+                            <div className="col-1">
+                                <button className="btn btn-warning px- "><i className="fas fa-trash-alt bg-warning fs-5"></i></button>
+                            </div>
+                            <div className="container-fluid">
+                                <ul className="list-unstyled row m-5">
+                                    {item.map((itemAdd, index) => {
+                                        return <MyPlan key={index} id={index} delete={deleteItem} text={itemAdd} />
+                                    })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Todo;
